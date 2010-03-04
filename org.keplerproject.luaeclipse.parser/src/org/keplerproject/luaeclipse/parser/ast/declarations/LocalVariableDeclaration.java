4c5c3eea38219b409344be62b9beec3646e11ce2
Kévin KIN-FOO <kkin-foo@sierrawireless.com>
2010-03-04 16:51:41 +0100

    Adding propoer declaration nodes

--- org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/ast/declarations/LocalVariableDeclaration.java
+++ org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/ast/declarations/LocalVariableDeclaration.java
@@ -14,13 +14,42 @@ package org.keplerproject.luaeclipse.parser.ast.declarations;
 import org.eclipse.dltk.ast.declarations.FieldDeclaration;
 import org.eclipse.dltk.ast.references.SimpleReference;
 
+/**
+ * Declaration of a local variable detected by outline and code assistance
+ * 
+ * @author Kevin KIN-FOO <kkin-foo@sierrawireless.com>
+ */
 public class LocalVariableDeclaration extends FieldDeclaration {
 
+    /**
+     * Initialize a local variable declaration node
+     * 
+     * @param name
+     *            name of the expression this variable is named after
+     * @param nameStart
+     *            start offset of name expression
+     * @param nameEnd
+     *            end offset of name expression
+     * @param start
+     *            start offset of variable body
+     * @param end
+     *            end offset of variable body
+     */
     public LocalVariableDeclaration(String name, int nameStart, int nameEnd,
 	    int declStart, int declEnd) {
 	super(name, nameStart, nameEnd, declStart, declEnd);
     }
 
+    /**
+     * Initialize a local variable declaration node
+     * 
+     * @param name
+     *            reference to the expression which is the name of the variable
+     * @param start
+     *            start offset of variable body
+     * @param end
+     *            end offset of variable body
+     */
     public LocalVariableDeclaration(SimpleReference name, int declStart,
 	    int declEnd) {
 	this(name.getName(), name.sourceStart(), name.sourceEnd(), declStart,
