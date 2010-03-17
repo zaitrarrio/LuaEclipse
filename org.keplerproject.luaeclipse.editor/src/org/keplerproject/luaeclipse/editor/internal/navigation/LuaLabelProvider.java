45e0a1bd2ed0f50f079af4eb4d46615e307b84eb
Kévin KIN-FOO <kkin-foo@sierrawireless.com>
2010-03-17 14:48:30 +0100

    Adding image fot private tables

--- org.keplerproject.luaeclipse.editor/src/org/keplerproject/luaeclipse/editor/internal/navigation/LuaLabelProvider.java
+++ org.keplerproject.luaeclipse.editor/src/org/keplerproject/luaeclipse/editor/internal/navigation/LuaLabelProvider.java
@@ -1,21 +1,15 @@
 package org.keplerproject.luaeclipse.editor.internal.navigation;
 
 import java.io.IOException;
-import java.net.URL;
 
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.dltk.ast.Modifiers;
 import org.eclipse.dltk.core.IMember;
 import org.eclipse.dltk.core.IType;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
 import org.keplerproject.luaeclipse.editor.Activator;
-import org.osgi.framework.Bundle;
 
 public class LuaLabelProvider extends LabelProvider {
-    private static Image image = null;
 
     @Override
     public String getText(Object element) {
@@ -48,13 +42,7 @@ public class LuaLabelProvider extends LabelProvider {
     }
 
     private Image getPrivateMethodIcon() throws IOException {
-	if (image == null) {
-	    Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
-	    URL url = bundle.getResource("/img/class_obj.gif");
-	    String file = FileLocator.resolve(url).getPath();
-	    image = new Image(Display.getDefault(), file);
-	}
-	return image;
+	return Activator.getImage("/img/class_obj.gif");
     }
 
 }
