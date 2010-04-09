668de0aae8f3033a8f36527cc695a2f4ffc55da7
Kévin KIN-FOO <kkin-foo@sierrawireless.com>
2010-04-09 18:33:05 +0200

    Starting to locate declarations with Metalua

--- plugins/org.keplerproject.luaeclipse.parser/scripts/ast_to_table.lua
+++ plugins/org.keplerproject.luaeclipse.parser/scripts/ast_to_table.lua
@@ -1,11 +1,75 @@
+-------------------------------------------------------------------------------
+--  Copyright (c) 2009 KeplerProject, Sierra Wireless.
+--  All rights reserved. This program and the accompanying materials
+--  are made available under the terms of the Eclipse Public License v1.0
+--  which accompanies this distribution, and is available at
+--  http://www.eclipse.org/legal/epl-v10.html
+-- 
+--  Contributors:
+--       Kevin KIN-FOO <kkin-foo@sierrawireless.com>
+--           - initial API and implementation and initial documentation
+---------------------------------------------------------------------------------
+local function dump( var )
+	local function _dump( t, spaces )
+		if type(t) == 'table' then 
+			for k,v in pairs(t) do
+				_dump(k, spaces.."\t")
+				_dump(v, spaces.."\t")
+			end
+		else
+			print (spaces .. type(t)..'('..tostring(t)..')')
+		end
+	end
+	_dump(var, "")
+end
+--
+-- Thoses are hashmaps to link nodes to their ID,
+-- in order to enable direct access, time efficient
+--
+local idToNode = {}
+local nodeToId = {}
+
 --
 -- Provides distinct IDs calls after calls
 -- @return int
 --
 local id = 0
-function getID()
+local function getID()
   id = id + 1
   return id
+end
+
+--
+--
+--
+local function matchDeclaration( ast )
+	--
+	--	Checks if node is already indexed in internal hashmaps
+	--	@param node	table Node to localize in hashmaps
+	--
+	local function registrated( node )
+		assert(type(node) == 'table', "Node of 'table' type expected.")
+		return nodeToId[ node ] ~= nil
+	end
+	local function store( node, occurrences )
+		assert( type(occurrences) == 'table' )
+		local id = nodeToId[ node ]
+		idToNode[ id ]['occurrences'] = occurrences
+	end 
+
+	-- Sort variables in source 
+	assert(
+		type(ast) == 'table',
+		"AST of 'table' type expected, "..type(ast).." found."
+	)
+	require 'metalua.walk.bindings'
+	local declared, leftovers = bindings( ast )
+	for declaration, occurrences in pairs(declared) do
+		if registrated(declaration) then
+			store(declaration, occurrences)
+			dump( occurrences )
+		end
+	end
 end
 
 --
@@ -16,38 +80,47 @@ end
 --
 -- @param ADT to index
 --
-local idToNode = {}
-local nodeToId = {}
 function index( ast )
-  -- Flush previous indexes
-  idToNode = {}
-  nodeToId = {}
-  doIndex( ast )
+ 	local function doIndex( adt )
+		local function childNodes( ast )
+			local nodes = {}
+	    	for k, v in ipairs( ast ) do
+	      		if type(v) == "table" then
+					nodes[ #nodes + 1 ] = v
+	      		end
+	    	end
+	    	return nodes
+	  	end
+ 		-- Index node
+		local id = getID()
+		idToNode[ id ] = adt
+	 	nodeToId[ adt ] = id
+
+		-- Index child nodes
+		for k,v in ipairs( childNodes(adt) )do
+	    	if ( type(v) == "table" ) then
+	      		doIndex( v )
+	    	end
+		end
+	end
+	--
+	-- Flush previous indexes to ensure consistance
+	--
+ 	idToNode = {}
+ 	nodeToId = {}
+
+ 	--
+ 	-- Reference nodes in hash table in order to have direct access
+	--
+ 	doIndex( ast )
+
+	--
+ 	-- Apprend declaration data on declaration nodes
+	--
+	matchDeclaration ( ast )
 end
-function doIndex( adt )
 
-  local function childNodes( ast )
-    nodes = {}
-    for k, v in ipairs( ast ) do
-      if ( type(v) == "table" ) then
-		nodes[ #nodes + 1 ] = v
-      end
-    end
-    return nodes
-  end
 
-  -- Index node
-  local id = getID()
-  idToNode[ id ] = adt
-  nodeToId[ adt ] = id
-
-  -- Index child nodes
-  for k,v in ipairs( childNodes(adt) )do
-    if ( type(v) == "table" ) then
-      doIndex( v )
-    end
-  end
-end
 
 --
 -- Retrurn children of a node
@@ -136,4 +209,34 @@ end
 -- 
 function hasLineInfo( id )
   return type(idToNode[ id ].lineinfo) == "table"
+end
+
+function hasDeclarations( id )
+	assert(type(id) == 'number', "Number expected '"..type(id).."' found.")
+	local doesNodeExists = type(idToNode[ id ]) == 'table'
+	return doesNodeExists and type(idToNode[ id ].occurrences) == 'table'
+end
+
+function getDeclarationsIDs( id )
+	assert(type(id) == 'number', "Number expected '"..type(id).."' found.")
+	-- Return empty table where no declarations are available
+	local ids = {}
+	if hasDeclarations(id) then
+		local declarations = idToNode[ id ].declarations
+		-- Search for nodes assotiated with declaration is hash map
+		dump(idToNode[ id ].occurrences)
+	end
+	return ids
+end
+--[[ ---------------------------------------------------------------------------
+]]
+local path = ";/home/kkinfoo/dev/luaEclipse/LuaEclipseM6/plugins/org.keplerproject.luaeclipse.metalua.32bits/"
+package.path = package.path ..path.."?.lua"..path.."?.luac"
+
+require 'metalua.compiler'
+local ast = mlc.luastring_to_ast( " local yy , xx= {}, function()end xx()" )
+
+index( ast )
+for k=0, id do
+	getDeclarationsIDs( k )
 end
