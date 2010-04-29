b97e6c00533a0d5a53a3d4742b489e405b67e510
Kévin KIN-FOO <kkin-foo@sierrawireless.com>
2010-04-29 15:20:17 +0200

    Renaming TestSuite 'Lua Eclipse' for runtime

--- tests/org.keplerproject.luaeclipse.tests/src/org/keplerproject/luaeclipse/tests/LDTSuite.java
+++ tests/org.keplerproject.luaeclipse.tests/src/org/keplerproject/luaeclipse/tests/LDTSuite.java
@@ -9,14 +9,6 @@
  *      Kevin KIN-FOO <kkin-foo@sierrawireless.com>
  *          - initial API and implementation and initial documentation
  *****************************************************************************/
-
-
-/**
- * @author	Kevin KIN-FOO <kkinfoo@anyware-tech.com>
- * @date $Date: 2009-06-15 17:55:03 +0200 (lun., 15 juin 2009) $
- * $Author: kkinfoo $
- * $Id: LDTSuite.java 1841 2009-06-15 15:55:03Z kkinfoo $
- */
 package org.keplerproject.luaeclipse.tests;
 
 import junit.framework.TestCase;
