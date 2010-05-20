12923e78484bcae08295e52b0776105d44ec1a68
Kévin KIN-FOO <kkin-foo@sierrawireless.com>
2010-05-20 11:08:00 +0200

    Starting to use our own Source element requestor ( a visitor in fact ), in order to enable attributes and back up code assistance

--- plugins/org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/LuaSourceElementParser.java
+++ plugins/org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/LuaSourceElementParser.java
@@ -9,13 +9,6 @@
  *      Kevin KIN-FOO <kkin-foo@sierrawireless.com>
  *          - initial API and implementation and initial documentation
  *****************************************************************************/
-
-/**
- * @author	Kevin KIN-FOO <kkinfoo@anyware-tech.com>
- * @date $Date: 2009-07-16 15:58:05 +0200 (jeu., 16 juil. 2009) $
- * $Author: kkinfoo $
- * $Id: LuaSourceElementParser.java 2086 2009-07-16 13:58:05Z kkinfoo $
- */
 package org.keplerproject.luaeclipse.parser;
 
 import org.eclipse.dltk.compiler.SourceElementRequestVisitor;
@@ -28,22 +21,22 @@ import org.keplerproject.luaeclipse.core.LuaNature;
  */
 public class LuaSourceElementParser extends AbstractSourceElementParser {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.dltk.core.AbstractSourceElementParser#createVisitor()
-     */
-    public SourceElementRequestVisitor createVisitor() {
-	return new SourceElementRequestVisitor(getRequestor());
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.dltk.core.AbstractSourceElementParser#createVisitor()
+	 */
+	public SourceElementRequestVisitor createVisitor() {
+		return new LuaSourceElementRequestor(getRequestor());
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.dltk.core.AbstractSourceElementParser#getNatureId()
-     */
-    @Override
-    protected String getNatureId() {
-	return LuaNature.LUA_NATURE;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.dltk.core.AbstractSourceElementParser#getNatureId()
+	 */
+	@Override
+	protected String getNatureId() {
+		return LuaNature.LUA_NATURE;
+	}
 }
