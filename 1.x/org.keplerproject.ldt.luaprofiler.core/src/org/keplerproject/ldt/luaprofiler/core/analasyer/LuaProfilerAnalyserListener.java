705028afd35fcd05a73bcb9d8117b992de07d0f6
Benjamin Cabé <BCabe@sierrawireless.com>
2011-05-20 22:51:27 +0200

    Housekeeping... remove 0.95 and 1.x old versions

--- 1.x/org.keplerproject.ldt.luaprofiler.core/src/org/keplerproject/ldt/luaprofiler/core/analasyer/LuaProfilerAnalyserListener.java
+++
@@ -1,7 +0,0 @@
-package org.keplerproject.ldt.luaprofiler.core.analasyer;
-
-import org.eclipse.debug.core.model.IProcess;
-
-public interface LuaProfilerAnalyserListener {
-	public void profilerDataChanged(IProcess process);
-}
--- prototype_dltk_0.95/org.keplerproject.dltk.ldt.core.parser/src/org/keplerproject/dltk/ldt/core/parser/ast/declarations/LuaArgument.java
+++
@@ -1,9 +0,0 @@
-package org.keplerproject.dltk.ldt.core.parser.ast.declarations;
-
-import org.eclipse.dltk.ast.declarations.Argument;
-
-public class LuaArgument extends Argument {
-	public LuaArgument() {
-		super();
-	}
-}
