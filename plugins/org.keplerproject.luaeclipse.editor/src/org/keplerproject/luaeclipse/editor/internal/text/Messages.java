6ad313a27bdc211530778e5da08c1e0956337f2b
Benjamin Cabé <BCabe@sierrawireless.com>
2011-05-23 17:47:27 +0200

    Update copyright headers

--- plugins/org.keplerproject.luaeclipse.editor/src/org/keplerproject/luaeclipse/editor/internal/text/Messages.java
+++ plugins/org.keplerproject.luaeclipse.editor/src/org/keplerproject/luaeclipse/editor/internal/text/Messages.java
@@ -1,14 +1,13 @@
-/******************************************************************************
- * Copyright (c) 2009 KeplerProject, Sierra Wireless.
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Sierra Wireless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *      Kevin KIN-FOO <kkin-foo@sierrawireless.com>
- *          - initial API and implementation and initial documentation
- *****************************************************************************/
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
 
 
 package org.keplerproject.luaeclipse.editor.internal.text;
--- plugins/org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/ast/statements/Return.java
+++ plugins/org.keplerproject.luaeclipse.parser/src/org/keplerproject/luaeclipse/parser/ast/statements/Return.java
@@ -1,14 +1,13 @@
-/******************************************************************************
- * Copyright (c) 2011 KeplerProject, Sierra Wireless.
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Sierra Wireless and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *      Kevin KIN-FOO <kkinfoo@sierrawireless.com>
- *          - initial API and implementation and initial documentation
- *****************************************************************************/
+ *     Sierra Wireless - initial API and implementation
+ *******************************************************************************/
 package org.keplerproject.luaeclipse.parser.ast.statements;
 
 import java.util.ArrayList;
