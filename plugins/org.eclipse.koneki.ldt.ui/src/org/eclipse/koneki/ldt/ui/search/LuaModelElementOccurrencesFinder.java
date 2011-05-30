dd6e3e130347173e79d28b5abb03eb2b4810df70
Kevin KIN-FOO <kkin-foo@sierrawireless.com>
2011-05-30 18:00:20 +0200

    Implementing variable highlight

---
+++ plugins/org.eclipse.koneki.ldt.ui/src/org/eclipse/koneki/ldt/ui/search/LuaModelElementOccurrencesFinder.java
@@ -0,0 +1,11 @@
+package org.eclipse.koneki.ldt.ui.search;
+
+import org.eclipse.dltk.ui.search.ModelElementOccurrencesFinder;
+
+public class LuaModelElementOccurrencesFinder extends ModelElementOccurrencesFinder {
+
+	public LuaModelElementOccurrencesFinder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
