62175dcdd931ffc7729e45a4377c064160d3d517
Benjamin Cabé <BCabe@sierrawireless.com>
2011-06-08 11:54:52 +0200

    Removed unneeded references to KeplerProject

--- tests/org.eclipse.koneki.ldt.parser.lua.tests/src/org/eclipse/koneki/ldt/parser/lua/tests/internal/TestIndex.java
+++ tests/org.eclipse.koneki.ldt.parser.lua.tests/src/org/eclipse/koneki/ldt/parser/lua/tests/internal/TestIndex.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2011 KeplerProject, Sierra Wireless.
+ * Copyright (c) 2011 Sierra Wireless.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
